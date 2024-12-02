a9fbf508c32bc4ce0f0fb0593512452fa25b75d7
Paolo De Angelis <paolo.deangelis@polito.it>
2024-12-02 17:03:41 +0100

    ➕ final databases cathodes

--- energy_gnome/main.py
+++ energy_gnome/main.py
@@ -2,6 +2,7 @@ from pathlib import Path  # noqa: F401
 
 from tqdm import tqdm  # noqa: F401
 import typer
+import os
 
 from energy_gnome.config import (  # noqa: F401
     BATTERY_TYPES,
@@ -15,6 +16,7 @@ from energy_gnome.dataset import get_raw_cathode, get_raw_perovskite
 from energy_gnome.dataset.cathodes import CathodeDatabase
 from energy_gnome.dataset.perovskites import PerovskiteDatabase
 from energy_gnome.utils.logger_config import logger, setup_logging  # noqa: F401
+from energy_gnome.dataset.temp_convert import process_data_with_yaml
 
 # Create the Typer app
 app = typer.Typer(help="Energy GNoME CLI.")
@@ -84,6 +86,14 @@ def pre_processing(something: str = typer.Argument(..., help="Data to preprocess
     logger.info("Preprocessing data...")
     pass
 
+@app.command()
+def convert(database_path: str = typer.Argument(help="Interim database to preprocess."), yaml_file: str = typer.Argument(help="Yaml configuration file.")):
+    df, final_output_path = process_data_with_yaml(database_path, yaml_file) 
+    out_dir = Path(os.path.dirname(final_output_path))
+    out_dir.mkdir(parents=True, exist_ok=True)
+    df.to_json(final_output_path)
+
+    
 
 def main():
     app()
